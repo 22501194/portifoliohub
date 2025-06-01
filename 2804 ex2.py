547e4b08f7c78b9152a64747de090f4e2e14fb6b
Alice de Sá <alicedesa1806@gmail.com>
2025-06-01 13:17:12 -0300

    Add files via upload

---
+++ 2804 ex2.py
@@ -0,0 +1,16 @@
+inicio = int(input("Digite o valor inicial em Fahrenheit: "))
+fim = int(input("Digite o valor final em Fahrenheit: "))
+
+print("Fahrenheit | Celsius")
+print("-----------------")
+
+
+if inicio <= fim:
+    for f in range(inicio, fim + 1):
+        c = 5 * (f - 32) / 9
+        print(f"{f:.1f} °F | {c:.3f} °C")
+
+else:
+    for f in range(inicio, fim - 1, -1):
+        c = 5 * (f - 32) / 9
+        print(f"{f:.1f} °F | {c:.3f} °C")
